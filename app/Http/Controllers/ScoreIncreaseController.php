eb800c5e6fd28c07c04efc40cbc56139a3f3795c
Juan Morales <jcamilo2696@gmail.com>
2024-05-09 16:52:03 -0500

    Created models with controller and migration file
    Added race, race features, score increases and traits models

---
+++ app/Http/Controllers/ScoreIncreaseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ScoreIncrease;
+use Illuminate\Http\Request;
+
+class ScoreIncreaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ScoreIncrease $scoreIncrease)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ScoreIncrease $scoreIncrease)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ScoreIncrease $scoreIncrease)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ScoreIncrease $scoreIncrease)
+    {
+        //
+    }
+}
