ed7b83f274d13bb809fea088258543d14e2818b6
Juan Camilo Morales <jcamilo2696@gmail.com>
2024-05-22 10:13:21 -0500

    Added endpoint route to update character class

--- routes/api.php
+++ routes/api.php
@@ -36,5 +36,6 @@ Route::controller(CharacterClassController::class)->group(function() {
         Route::post('/create', 'store');
         Route::get('/list', 'index');
         Route::get('/list/details/{id}', 'show');
+        Route::patch('/update/{id}', 'update');
     });
 });
